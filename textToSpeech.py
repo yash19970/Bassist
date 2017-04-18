7e1b1b77ad9ed63be55d88fcf441a22699123d2e
yash19970 <yash_19@outlook.com>
2017-04-19 03:44:22 +0530

    textToSpeech almost done, scraping only regex part remaining.

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,4 +1,17 @@
 import pyttsx
+from bs4 import BeautifulSoup as bs
+import urllib2
+
+string ="dog" 
+side_addr = 'https://en.wikipedia.org/wiki/'+string
+response = urllib2.urlopen(side_addr)
+html = response.read()
+soup = bs(html,'lxml')
+for div in soup.findAll('div', attrs={'id':'mw-content-text','class':'mw-content-ltr'}):
+    #print div.p.text[0:150] #wikipedia data.
+	p = div.p.text[10:100]
+
+#print p
 engine = pyttsx.init()
-engine.say('This is generally used for data structuers')
+engine.say(p)
 engine.runAndWait()
--- wikiScrap.py
+++ wikiScrap.py
@@ -5,3 +5,5 @@ side_addr = 'https://en.wikipedia.org/wiki/'+string
 response = urllib2.urlopen(side_addr)
 html = response.read()
 soup = bs(html,'lxml')
+for div in soup.findAll('div', attrs={'id':'mw-content-text','class':'mw-content-ltr'}):
+    print div.p.text[0:150] #wikipedia data.
